c674aae20122bd29f28a2e67adea4bd3bf0c7c78
vanshikasukhija <vsukhija205@gmial.com>
2025-08-03 16:53:23 +0530

    Initial project files with React components and setup

---
+++ navroute/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-green-600 p-4'>React Router</h1>
+    </>
+  )
+}
+
+export default App
---
+++ navroute/src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
